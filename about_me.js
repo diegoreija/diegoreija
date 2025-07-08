d592cfbad45091f972cbca77a269e9e01e9737f2
diegoreija <diegoreija21@gmail.com>
2025-07-08 22:19:52 +0200

    media querry 1

--- about_me.js
+++ about_me.js
@@ -1,11 +1,16 @@
+function showChapter(num) {
+  // Ocultar todos los capítulos
+  document.querySelectorAll('.chapter').forEach(el => el.style.display = 'none');
+  // Mostrar el capítulo activo
+  document.getElementById('chapter' + num).style.display = 'block';
+   // Actualizar botones activos
+  document.querySelectorAll('.tab').forEach(btn => btn.classList.remove('active'));
+  document.querySelectorAll('.tab')[num - 1].classList.add('active');
+}
 
-  function showChapter(num) {
-    // Ocultar todos los capítulos
-    document.querySelectorAll('.chapter').forEach(el => el.style.display = 'none');
-    // Mostrar el capítulo activo
-    document.getElementById('chapter' + num).style.display = 'block';
+// --- JavaScript ---
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
 
-    // Actualizar botones activos
-    document.querySelectorAll('.tab').forEach(btn => btn.classList.remove('active'));
-    document.querySelectorAll('.tab')[num - 1].classList.add('active');
-  }
